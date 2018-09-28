89c7bbdae14be089f703c0d5b6e1ab3a8edbb0ed
Evgeniy <samarin_ey@hotmail.com>
2018-09-28 14:11:12 +0900

    Реализация программного добавления строки в списке

--- app/src/main/java/com/dogvscat/retingall/MainActivity.kt
+++ app/src/main/java/com/dogvscat/retingall/MainActivity.kt
@@ -4,23 +4,41 @@ import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.CardView
+import android.util.AttributeSet
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import at.grabner.circleprogress.CircleProgressView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var layoutMain: View
+    private lateinit var layoutMain: View
+    private lateinit var viewLinearCard: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        //получаем ссылки на элементы графического интерфейса
         layoutMain = findViewById(R.id.layout_activity_main)
+        viewLinearCard = findViewById(R.id.view_linear_card)
 
         findViewById<FloatingActionButton>(R.id.fab_add).setOnClickListener {
+            //создаем CardView пустышку
+           // val viewCardItem = CardView(this)
+            //val viewTextItem = TextView(this)
+
+            //val attrItem: AttributeSet
+//Нужно разобраться с Атрибутами
+            //val viewCircleProgressItem = CircleProgressView(this,)
+
+
+
             Snackbar.make(layoutMain, "Test", Snackbar.LENGTH_SHORT).show()
         }
     }
