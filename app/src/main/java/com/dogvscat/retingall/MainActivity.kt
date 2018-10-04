986f1014de8b76bd389ababc031c23cfbae5952d
Evgeniy <samarin_ey@hotmail.com>
2018-10-04 15:20:16 +0900

    Отдельная функция для добавления карточки + включено документирование кода на русском языке в формате KDoc

--- app/src/main/java/com/dogvscat/retingall/MainActivity.kt
+++ app/src/main/java/com/dogvscat/retingall/MainActivity.kt
@@ -43,78 +43,8 @@ class MainActivity : AppCompatActivity() {
         viewLinearCard = findViewById(R.id.view_linear_card)
 
         findViewById<FloatingActionButton>(R.id.fab_add).setOnClickListener {
-            //создаем CardView пустышку
-            val viewCardItem = CardView(this)
-            // Initialize a new LayoutParams instance, CardView width and height
-            val layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT, // CardView width
-                    LinearLayout.LayoutParams.WRAP_CONTENT // CardView height
-            )
-            layoutParams.setMargins(5.toPx(), 5.toPx(), 5.toPx(), 5.toPx())
-            viewCardItem.layoutParams = layoutParams
-            viewCardItem.radius = 5.toPx().toFloat()
-            // Set the card view content padding
-            viewCardItem.setContentPadding(5.toPx(), 5.toPx(), 5.toPx(), 5.toPx())
-            // Set the card view background color
-            viewCardItem.setCardBackgroundColor(getColor(R.color.colorListItemBG))
-
-            //Делаем Layout для корректного добавления элементов
-            val lineralLayout = LinearLayout(this)
-            lineralLayout.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT, // width
-                    LinearLayout.LayoutParams.WRAP_CONTENT // height
-            )
-            lineralLayout.orientation = LinearLayout.HORIZONTAL
-
-            //Создаем текстовое поле с названием позиции и добавляем его на карточку
-            val viewTextItem = TextView(this)
-            val layoutParamsText = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT, // width
-                    LinearLayout.LayoutParams.MATCH_PARENT, // height
-                    1F
-            )
-            viewTextItem.layoutParams = layoutParamsText
-            viewTextItem.gravity = Gravity.START or Gravity.CENTER
-            viewTextItem.text = getString(R.string.string_test_short_rus)
-
-            //Создаем красивый прогресс бар и добавляем его на карточку
-            val viewCircleProgressItem = CircleProgressView(this, null)
-            // Initialize a new LayoutParams instance, CircleProgressView width and height
-            val layoutParamsCircle = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT, // width
-                    LinearLayout.LayoutParams.WRAP_CONTENT // height
-            )
-            //layoutParamsCircle.setMargins(5.toPx(), 5.toPx(), 5.toPx(), 5.toPx())
-            layoutParamsCircle.height = 50.toPx()
-            viewCircleProgressItem.layoutParams = layoutParamsCircle
-
-            //специфичные стили
-            viewCircleProgressItem.isAutoTextSize = true
-            viewCircleProgressItem.setBarColor(getColor(R.color.colorPrimary))
-            viewCircleProgressItem.barWidth = 4.toPx()
-            viewCircleProgressItem.innerContourSize = 0F
-            viewCircleProgressItem.maxValue = 100F
-            viewCircleProgressItem.outerContourSize = 0F
-            viewCircleProgressItem.rimColor = getColor(R.color.colorAccent)
-            viewCircleProgressItem.rimWidth = 4.toPx()
-            viewCircleProgressItem.isSeekModeEnabled = true
-            viewCircleProgressItem.setSpinBarColor(getColor(R.color.colorPrimary))
-            viewCircleProgressItem.setTextColor(getColor(R.color.colorProgressBarText))
-            viewCircleProgressItem.textScale = 1F
-            viewCircleProgressItem.unit = "rp"
-            viewCircleProgressItem.setUnitColor(getColor(R.color.colorProgressBarText))
-            viewCircleProgressItem.setUnitPosition(UnitPosition.RIGHT_TOP)
-            viewCircleProgressItem.unitScale = 1F
-            viewCircleProgressItem.isUnitVisible = true
-            viewCircleProgressItem.setValue(25F)
-
-            //набрасываем созданное на наши view
-            lineralLayout.addView(viewTextItem)
-            lineralLayout.addView(viewCircleProgressItem)
-            viewCardItem.addView(lineralLayout)
-
             //выводим на экран cardView
-            viewLinearCard.addView(viewCardItem)
+            viewLinearCard.addView(createCardViewItem(getString(R.string.string_test_short_eng),20F))
             Log.d(LOGDEBUGTAG, "Добавили новую карточку")
         }
 
@@ -122,6 +52,88 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, StudyActivity::class.java))
             Log.d(LOGDEBUGTAG, "Перешли на страницу для тестов")
         }
+    }
+
+    /**
+     * Функиця для создания новой карточки с указанием названия и текущего уровня респекта
+     * @author Самарин Е
+     * @return Возвращяет созданную карточку CardView
+     * @param title указывает наименование добавляемой позиции
+     * @param respect указывает текущий уровень рейтинга
+     */
+    private fun createCardViewItem(title: String, respect: Float) : CardView{
+
+        //создаем CardView пустышку
+        val viewCardItem = CardView(this)
+        // Initialize a new LayoutParams instance, CardView width and height
+        val layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, // CardView width
+                LinearLayout.LayoutParams.WRAP_CONTENT // CardView height
+        )
+        layoutParams.setMargins(5.toPx(), 5.toPx(), 5.toPx(), 5.toPx())
+        viewCardItem.layoutParams = layoutParams
+        viewCardItem.radius = 5.toPx().toFloat()
+        // Set the card view content padding
+        viewCardItem.setContentPadding(5.toPx(), 5.toPx(), 5.toPx(), 5.toPx())
+        // Set the card view background color
+        viewCardItem.setCardBackgroundColor(getColor(R.color.colorListItemBG))
+
+        //Делаем Layout для корректного добавления элементов
+        val lineralLayout = LinearLayout(this)
+        lineralLayout.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, // width
+                LinearLayout.LayoutParams.WRAP_CONTENT // height
+        )
+        lineralLayout.orientation = LinearLayout.HORIZONTAL
+
+        //Создаем текстовое поле с названием позиции и добавляем его на карточку
+        val viewTextItem = TextView(this)
+        val layoutParamsText = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, // width
+                LinearLayout.LayoutParams.MATCH_PARENT, // height
+                1F
+        )
+        viewTextItem.layoutParams = layoutParamsText
+        viewTextItem.gravity = Gravity.START or Gravity.CENTER
+        viewTextItem.text = title
+
+        //Создаем красивый прогресс бар и добавляем его на карточку
+        val viewCircleProgressItem = CircleProgressView(this, null)
+        // Initialize a new LayoutParams instance, CircleProgressView width and height
+        val layoutParamsCircle = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, // width
+                LinearLayout.LayoutParams.WRAP_CONTENT // height
+        )
+        //layoutParamsCircle.setMargins(5.toPx(), 5.toPx(), 5.toPx(), 5.toPx())
+        layoutParamsCircle.height = 50.toPx()
+        viewCircleProgressItem.layoutParams = layoutParamsCircle
+
+        //специфичные стили
+        viewCircleProgressItem.isAutoTextSize = true
+        viewCircleProgressItem.setBarColor(getColor(R.color.colorPrimary))
+        viewCircleProgressItem.barWidth = 4.toPx()
+        viewCircleProgressItem.innerContourSize = 0F
+        viewCircleProgressItem.maxValue = 100F
+        viewCircleProgressItem.outerContourSize = 0F
+        viewCircleProgressItem.rimColor = getColor(R.color.colorAccent)
+        viewCircleProgressItem.rimWidth = 4.toPx()
+        viewCircleProgressItem.isSeekModeEnabled = true
+        viewCircleProgressItem.setSpinBarColor(getColor(R.color.colorPrimary))
+        viewCircleProgressItem.setTextColor(getColor(R.color.colorProgressBarText))
+        viewCircleProgressItem.textScale = 1F
+        viewCircleProgressItem.unit = "rp"
+        viewCircleProgressItem.setUnitColor(getColor(R.color.colorProgressBarText))
+        viewCircleProgressItem.setUnitPosition(UnitPosition.RIGHT_TOP)
+        viewCircleProgressItem.unitScale = 1F
+        viewCircleProgressItem.isUnitVisible = true
+        viewCircleProgressItem.setValue(respect)
+
+        //набрасываем созданное на наши view
+        lineralLayout.addView(viewTextItem)
+        lineralLayout.addView(viewCircleProgressItem)
+        viewCardItem.addView(lineralLayout)
+
+        return viewCardItem
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
