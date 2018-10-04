0c3054fdcfa4450cb902d1f85d9113cdd8e4fbb6
Samarin.E.Home <samarin_ey@hotmail.com>
2018-10-04 22:22:04 +0900

    Исправлены ошибки возникшие из-за вставки стороннего кода... - имеются проблемы при inflate Fresco View - нужно поискать решение

--- app/src/main/java/com/dogvscat/retingall/AddActivity.kt
+++ app/src/main/java/com/dogvscat/retingall/AddActivity.kt
@@ -1,13 +1,19 @@
 package com.dogvscat.retingall
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
-import android.webkit.PermissionRequest
+import butterknife.ButterKnife
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.common.ResizeOptions
+import com.facebook.imagepipeline.request.ImageRequestBuilder
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -15,17 +21,24 @@ import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_add.*
 import kotlinx.android.synthetic.main.app_bar.*
+import java.io.File
 
 class AddActivity : AppCompatActivity() {
     private val LOGDEBUGTAG: String = "POINT"
+    private val TAKE_PHOTO_REQUEST: Int = 0
+    lateinit var mCurrentPhotoPath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fresco.initialize(this)
         setContentView(R.layout.activity_add)
 
         //включаем toolbar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //не особо представляю зачем это взято из примера
+        ButterKnife.bind(this)
 
         but_submit.setOnClickListener {
             val intent = Intent()
@@ -44,16 +57,13 @@ class AddActivity : AppCompatActivity() {
     private fun validatePermissions() {
         Dexter.withActivity(this)
                 .withPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .withListener(object: PermissionListener {
-                    override fun onPermissionGranted(
-                            response: PermissionGrantedResponse?) {
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
                         launchCamera()
                     }
 
-                    override fun onPermissionRationaleShouldBeShown(
-                            permission: PermissionRequest?,
-                            token: PermissionToken?) {
-                        AlertDialog.Builder(this@MainActivity)
+                    override fun onPermissionRationaleShouldBeShown(permission: com.karumi.dexter.listener.PermissionRequest?, token: PermissionToken?) {
+                        AlertDialog.Builder(this@AddActivity)
                                 .setTitle(
                                         R.string.storage_permission_rationale_title)
                                 .setMessage(
@@ -70,19 +80,78 @@ class AddActivity : AppCompatActivity() {
                                             token?.continuePermissionRequest()
                                         })
                                 .setOnDismissListener({
-                                    token?.cancelPermissionRequest() })
+                                    token?.cancelPermissionRequest()
+                                })
                                 .show()
                     }
 
                     override fun onPermissionDenied(
                             response: PermissionDeniedResponse?) {
-                        Snackbar.make(mainContainer!!,
+                        Snackbar.make(layout_add_main!!,
                                 R.string.storage_permission_denied_message,
                                 Snackbar.LENGTH_LONG)
                                 .show()
                     }
                 })
                 .check()
+    }
+
+    /**
+     * Ипользуем камеру
+     */
+    private fun launchCamera() {
+        val values = ContentValues(1)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
+        val fileUri = contentResolver
+                .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        values)
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            mCurrentPhotoPath = fileUri.toString()
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+            startActivityForResult(intent, TAKE_PHOTO_REQUEST)
+        }
+    }
+
+    /**
+     * Проверяем, получено ли фото
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int,
+                                  data: Intent?) {
+        if (resultCode == Activity.RESULT_OK
+                && requestCode == TAKE_PHOTO_REQUEST) {
+            processCapturedPhoto()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    /**
+     * обрабатываем полученное фото
+     */
+    private fun processCapturedPhoto() {
+        val cursor = contentResolver.query(Uri.parse(mCurrentPhotoPath),
+                Array(1) { android.provider.MediaStore.Images.ImageColumns.DATA },
+                null, null, null)
+        cursor.moveToFirst()
+        val photoPath = cursor.getString(0)
+        cursor.close()
+        val file = File(photoPath)
+        val uri = Uri.fromFile(file)
+
+        val height = resources.getDimensionPixelSize(R.dimen.photo_height)
+        val width = resources.getDimensionPixelSize(R.dimen.photo_width)
+
+        val request = ImageRequestBuilder.newBuilderWithSource(uri)
+                .setResizeOptions(ResizeOptions(width, height))
+                .build()
+        val controller = Fresco.newDraweeControllerBuilder()
+                .setOldController(view_simple_image?.controller)
+                .setImageRequest(request)
+                .build()
+        view_simple_image?.controller = controller
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
