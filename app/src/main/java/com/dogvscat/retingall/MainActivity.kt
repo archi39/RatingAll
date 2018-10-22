2eea5c88a1f51610b65f786397818ecf32ffb784
Samarin.E.Home <samarin_ey@hotmail.com>
2018-10-22 20:42:50 +0900

    Добавили Roadmap в preview страницы

--- app/src/main/java/com/dogvscat/retingall/MainActivity.kt
+++ app/src/main/java/com/dogvscat/retingall/MainActivity.kt
@@ -3,10 +3,8 @@ package com.dogvscat.retingall
 import android.content.Intent
 import android.content.res.Resources
 import android.os.Bundle
-import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
-import android.support.design.widget.SwipeDismissBehavior
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
 import android.util.Log
@@ -34,7 +32,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Инициализируем библотеку для работы с фото
-        Fresco.initialize(this);
+        Fresco.initialize(this)
         setContentView(R.layout.activity_main)
         //add toolbar to the activity
         setSupportActionBar(toolbar)
@@ -158,7 +156,7 @@ class MainActivity : AppCompatActivity() {
         lineralLayout.addView(viewCircleProgressItem)
         viewCardItem.addView(lineralLayout)
         //делаем карточку свайпной
-        implementSwipeDismiss(viewCardItem)
+//        implementSwipeDismiss(viewCardItem)
 
         return viewCardItem
     }
@@ -197,14 +195,14 @@ class MainActivity : AppCompatActivity() {
     /**
      * method to implement swipe dismiss feature over card view
      **/
-    private fun implementSwipeDismiss(cardView: CardView) {
-        val swipeDismissBehavior = SwipeDismissBehavior<View>()
-        //Swipe direction i.e any direction, here you can put any direction LEFT or RIGHT
-        swipeDismissBehavior.setSwipeDirection(SwipeDismissBehavior.SWIPE_DIRECTION_ANY)
-        val layoutParams: CoordinatorLayout.LayoutParams = cardView.layoutParams as CoordinatorLayout.LayoutParams
-        //set swipe behaviour to Coordinator layout
-        layoutParams.behavior = swipeDismissBehavior
-    }
+//    private fun implementSwipeDismiss(cardView: CardView) {
+//        val swipeDismissBehavior = SwipeDismissBehavior<View>()
+//        //Swipe direction i.e any direction, here you can put any direction LEFT or RIGHT
+//        swipeDismissBehavior.setSwipeDirection(SwipeDismissBehavior.SWIPE_DIRECTION_ANY)
+//        val layoutParams: CoordinatorLayout.LayoutParams = cardView.layoutParams as CoordinatorLayout.LayoutParams
+//        //set swipe behaviour to Coordinator layout
+//        layoutParams.behavior = swipeDismissBehavior
+//    }
 
     //функция приеобразования Px to Dp
     fun Int.toDp(): Int = (this / Resources.getSystem().displayMetrics.density).toInt()
