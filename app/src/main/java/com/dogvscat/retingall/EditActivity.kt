c0c3d3bd4585c4375942e4ef8dbc5d51406abb93
Samarin.E.Home <samarin_ey@hotmail.com>
2018-10-30 00:40:14 +0900

    Добавил обработчик для кнопки редактировать - нужно включить перерисовку при возврате в главное окно

--- app/src/main/java/com/dogvscat/retingall/EditActivity.kt
+++ app/src/main/java/com/dogvscat/retingall/EditActivity.kt
@@ -1,13 +1,16 @@
 package com.dogvscat.retingall
 
+import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import kotlinx.android.synthetic.main.app_bar.*
+
 
 class EditActivity : AppCompatActivity() {
     private lateinit var itemId: String
@@ -30,9 +33,27 @@ class EditActivity : AppCompatActivity() {
 
         //получаем ссылки на элементы графического интерфейса
         layoutActivityEdit = findViewById(R.id.layout_activity_edit)
-        editTextTitle =  findViewById(R.id.edit_text_title)
-        editTextNumber =  findViewById(R.id.edit_text_number)
+        editTextTitle = findViewById(R.id.edit_text_title)
+        editTextNumber = findViewById(R.id.edit_text_number)
+
         initElement()
+
+        findViewById<Button>(R.id.btn_end).setOnClickListener {
+            // создаем объект для данных
+            val contentValues = ContentValues()
+            Log.d(LOGDEBUGTAG, "--- Update ${DBHelper.TABLE_ITEMS}: ---")
+            // подготовим значения для обновления
+            contentValues.put(DBHelper.KEY_TITLE, editTextTitle.text.toString())
+            contentValues.put(DBHelper.KEY_RATING, editTextNumber.text.toString().toFloat())
+            // обновляем по id
+            database.update(DBHelper.TABLE_ITEMS,
+                    contentValues,
+                    DBHelper.KEY_ID + " = ? ",
+                    arrayOf(itemId))
+            database.close()
+            //закрываем активити
+            finish()
+        }
     }
 
     private fun initElement() {
@@ -53,11 +74,8 @@ class EditActivity : AppCompatActivity() {
                 //наполняем наш список элементами
                 editTextTitle.setText(title, TextView.BufferType.EDITABLE)
                 editTextNumber.setText(rating, TextView.BufferType.EDITABLE)
-
             } while (cursor.moveToNext())
         } else {
-            Snackbar.make(layoutActivityEdit, R.string.action_empty_db, Snackbar.LENGTH_SHORT)
-                    .show()
         }
 
         cursor.close()
